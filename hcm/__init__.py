9ca4cd7e95e54e651b03fbcd1f9899b63dc70c18
Alex Merose <al@merose.com>
2025-01-02 13:52:31 -0800

    Fix: bish was broken due to import order.

--- hcm/__init__.py
+++ hcm/__init__.py
@@ -1,8 +1,10 @@
 from . import dynamical
 from . import io
 from . import types
-from .bish import cli
 from .const import *
 from .music import *
 from .sample import *
 from .synth import *
+
+# must be imported last!
+from .bish import cli
